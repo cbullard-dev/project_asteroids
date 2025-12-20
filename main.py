77996854ae27954e8e8fbe3bb35c256e8562f918
Cal <60569006+cbullard-dev@users.noreply.github.com>
2025-12-20 21:28:27 +0900

    Added the clock and delta time variables to be able to run the game at a relative speed

--- main.py
+++ main.py
@@ -2,15 +2,18 @@ import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 
-pygame.init()
-screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
-
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"""
     Screen width: {SCREEN_WIDTH}
     Screen height: {SCREEN_HEIGHT}
     """)
+
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    delta_time = 0
+
     while True:
         log_state()
         for event in pygame.event.get():
@@ -18,6 +21,7 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        delta_time = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
