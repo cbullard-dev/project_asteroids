7ddffa8925cd2290a8d58e913a2ea4c64200c622
Cal <60569006+cbullard-dev@users.noreply.github.com>
2025-12-20 20:59:35 +0900

    Updated the gitignore to remove an accidentally included file Added new logger file created by the boot.dev team for the project Added new print to the main method of the main class for debugging the new contants file

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import pygame
-import constants
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print(f"Screen width = {constants.SCREEN_WIDTH}\nScreen height = {constants.SCREEN_WIDTH}")
+    print(f"""
+    Screen width: {SCREEN_WIDTH}
+    Screen height: {SCREEN_HEIGHT}
+    """)
 
 
 if __name__ == "__main__":
