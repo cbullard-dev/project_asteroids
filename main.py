0e3ff28ad16723ddbae8efb900e164298240601d
Cal <60569006+cbullard-dev@users.noreply.github.com>
2025-12-25 19:42:29 +0900

    Added simple collision logic for shots and ateroids to kill asteroids when they are hit with a shot

--- main.py
+++ main.py
@@ -42,6 +42,11 @@ def main():
                 log_event("player_hit")
                 print("Game over!")
                 sys.exit()
+            for shot in shots:
+                if shot.collides_with(asteroid):
+                    log_event("asteroid_shot")
+                    shot.kill()
+                    asteroid.kill()
         for item in drawable:
             item.draw(screen)
         pygame.display.flip()
